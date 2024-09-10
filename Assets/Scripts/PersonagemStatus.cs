f0635d914c9a707708bdb73f9bff31a1b567007c
Matheus Ferreira <89048083+matara061@users.noreply.github.com>
2024-09-10 09:11:01 -0300

    personagens status inicio

---
+++ Assets/Scripts/PersonagemStatus.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonagemStatus : MonoBehaviour
+{
+    // status
+    public int _life;
+    public int _damage;
+    public int _defense;
+    public int _magicDamage;
+    public int _magicDefense;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
