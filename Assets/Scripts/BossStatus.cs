0e55f8b98ab5fd2a750bf476b99a6e2fff956802
Matheus Ferreira <89048083+matara061@users.noreply.github.com>
2024-09-10 12:00:56 -0300

    UI inicio, status ajustes

---
+++ Assets/Scripts/BossStatus.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossStatus : MonoBehaviour
+{
+
+    public int _life;
+    public int _damage;
+    public int _defense;
+    public int _magicDamage;
+    public int _magicDefense;
+
+    public int _currentLife;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReciveDamage(int damage, bool magic)
+    {
+        if (magic)
+        {
+            damage /= _magicDefense;
+            _life -= damage;
+        }
+        else
+        {
+            damage /= _defense;
+            _life -= damage;
+        }
+    }
+}
--- Assets/Scripts/PersonagemStatus.cs
+++ Assets/Scripts/PersonagemStatus.cs
@@ -4,21 +4,72 @@ using UnityEngine;
 
 public class PersonagemStatus : MonoBehaviour
 {
+
+    [SerializeField]
+    GameManager gameManager;
     // status
     public int _life;
     public int _damage;
     public int _defense;
     public int _magicDamage;
     public int _magicDefense;
+    public int _moveArea; // quantos tiles pode mover por rodada 
+
+    private int _blessingEfect;
+
+    public int _currentLife;
 
     void Start()
     {
+        if (gameManager == null)
+        {
+            gameManager = FindAnyObjectByType<GameManager>();
+        }
+
+        if(gameManager !=  null)
+        {
+            BlessingEfect();
+        }
         
+        if(_blessingEfect != 0)
+        {
+            _life *= _blessingEfect;
+            _damage *= _blessingEfect;
+            _defense *= _blessingEfect;
+            _magicDamage *= _blessingEfect;
+            _magicDefense *= _blessingEfect;
+        }
+
+        _currentLife = _life;
     }
 
-    // Update is called once per frame
-    void Update()
+    void BlessingEfect()
     {
-        
+        if (gameManager._blessing[2])
+        {
+            _blessingEfect = 4;
+        }
+        else if (gameManager._blessing[1])
+        {
+            _blessingEfect = 3;
+        }
+        else if (gameManager._blessing[0])
+        {
+            _blessingEfect = 2;
+        }
+    }
+
+    public void ReciveDamage(int damage, bool magic)
+    {
+        if(magic)
+        {
+            damage /= _magicDefense;
+            _currentLife -= damage;
+        }
+        else
+        {
+            damage /= _defense;
+            _currentLife -= damage;
+        }
     }
 }
